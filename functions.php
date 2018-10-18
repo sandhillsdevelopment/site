be5d8c284389db6b996d0ad2f951e73c2d82673d
Sean Davis <sdavis2702@gmail.com>
2018-10-18 11:45:59 -0700

    fix borked functions file

--- functions.php
+++ functions.php
@@ -1,20 +1,16 @@
 <?php
 /**
- * Themedd child theme.
+ * Sandhills Development child theme.
  */
-function themedd_child_styles() {
+function sd_styles() {
 
-    $parent_style = 'themedd';
+	$parent_style = 'themedd';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'themedd-child',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get( 'Version' )
-    );
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'sd-child',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
 }
-add_action( 'wp_enqueue_scripts', 'themedd_child_styles' );
-
-/**
- * Place any custom functionality/code snippets below.
- */
+add_action( 'wp_enqueue_scripts', 'sd_styles' );
