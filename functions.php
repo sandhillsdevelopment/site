4905081e6f635a1fbea30a6458de450bad5ba7d7
Sean Davis <sdavis2702@gmail.com>
2019-10-15 11:34:42 -0700

    version bump 1.0.2

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 /**
  * Definitions
  */
-define( 'SHD_SITE_VERSION', '1.0.1' );
+define( 'SHD_SITE_VERSION', '1.0.2' );
 
 define( 'SHD_ROOT', dirname(__FILE__) );
 define( 'SHD_INCLUDES', SHD_ROOT . '/includes/' );
