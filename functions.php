4dea6c8a20b7e27e342438eb213382585c090ee8
Sean Davis <sdavis2702@gmail.com>
2020-01-14 11:03:48 -0800

    version bump 1.0.4

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 /**
  * Definitions
  */
-define( 'SHD_SITE_VERSION', '1.0.3' );
+define( 'SHD_SITE_VERSION', '1.0.4' );
 
 define( 'SHD_ROOT', dirname(__FILE__) );
 define( 'SHD_INCLUDES', SHD_ROOT . '/includes/' );
