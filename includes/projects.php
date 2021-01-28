c1de51e82925443fc87ee3544c07972629feaef1
Sean Davis <sdavis2702@gmail.com>
2021-01-28 10:47:07 -0600

    change data source for company Projects #50

--- includes/projects.php
+++ includes/projects.php
@@ -3,6 +3,20 @@
  * Functions specific to our projects
  */
 
+/**
+ * All company projects
+ */
+function shd_get_projects() {
+
+	$project_args = array(
+		'post_type'      => 'project',
+		'post_status'    => 'publish',
+		'posts_per_page' => -1,
+		'order'          => 'ASC',
+	);
+	return get_posts( $project_args );
+}
+
 
 /**
  * Get all of our projects.
